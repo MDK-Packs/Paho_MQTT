09cffe0f4ee04ff6e9b2abe859ac2e4f9ebfb920
Robert Rostohar <Robert.Rostohar@arm.com>
2019-04-02 10:00:51 +0200

    Updated pack generator script

--- gen_pack.sh
+++ gen_pack.sh
@@ -96,7 +96,7 @@ rm -rf $PACK_BUILD/.settings
 rm -rf $PACK_BUILD/Debug
 
 # Run Pack Check and generate PackName file
-$UTILITIES_DIR/$UTILITIES_OS/PackChk.exe $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc -n PackName.txt
+$UTILITIES_DIR/$UTILITIES_OS/PackChk.exe $PACK_BUILD/$PACK_VENDOR.$PACK_NAME.pdsc -n PackName.txt -x M362 -x M382
 errorlevel=$?
 
 if [ $errorlevel -ne 0 ]; then
