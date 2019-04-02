76c17d6e440e4aa6624aa864c23a742afc0f3576
Robert Rostohar <Robert.Rostohar@arm.com>
2019-04-02 15:58:37 +0200

    Added missing include for mbedTLS debug

--- contributions/add/MQTTClient-C/src/mdk/MQTTPlatform.h
+++ contributions/add/MQTTClient-C/src/mdk/MQTTPlatform.h
@@ -21,6 +21,7 @@
 #include "iot_socket.h"
 #include "RTE_Components.h"
 #ifdef    RTE_Security_mbedTLS
+#include "mbedtls/debug.h"
 #include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
 #include "mbedtls/ssl.h"
